84326885da94fd50d4d582fc3787eddac48715bb
Lic <121096190@qq.com>
2020-07-06 13:03:42 +0800

    Update ImmersionUtil.kt
    添加注释

--- immersion/src/main/java/com/lllic/immersion/ImmersionUtil.kt
+++ immersion/src/main/java/com/lllic/immersion/ImmersionUtil.kt
@@ -22,6 +22,9 @@ fun Window.immersive(statusBarAlpha: Int = 0, navigationBarAlpha: Int = 0) {
     immersiveNavigationBar(navigationBarAlpha)
 }
 
+/**
+* 透明状态栏，并将跟布局延申到状态栏下
+*/
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 fun Window.immersiveStatusBar(alpha: Int = 0) {
     setStatusBarTranslucent(alpha)
@@ -33,11 +36,17 @@ fun Window.immersiveNavigationBar(alpha: Int = 0) {
     setNavigationBarTranslucent(this, alpha)
 }
 
+/**
+* 设置状态栏透明度
+*/
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 private fun Window.setStatusBarTranslucent(alpha: Int = 0) {
     colorStatusBar(Color.argb(alpha, 0, 0, 0))
 }
 
+/**
+* 设置状态栏颜色
+*/
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 fun Window.colorStatusBar(color: Int) {
     addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
@@ -45,6 +54,9 @@ fun Window.colorStatusBar(color: Int) {
 
 }
 
+/**
+* 将跟布局延申到状态栏下
+*/
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 fun Window.overlayStatusBar(overlay: Boolean = true) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
@@ -66,11 +78,17 @@ fun Window.overlayStatusBar(overlay: Boolean = true) {
     }
 }
 
+/**
+* 将view添加状态栏高度的topMargin
+*/
 fun View.fitStatusBar() {
     val params: MarginLayoutParams = layoutParams as MarginLayoutParams
     params.topMargin = params.topMargin + getStatusBarHeight()
 }
 
+/**
+* 将view的增加状态栏高度的 height和paddingTop
+*/
 fun View.fitStatusBarOverlay() {
     val params: MarginLayoutParams = layoutParams as MarginLayoutParams
     if (params.height == 0) {
